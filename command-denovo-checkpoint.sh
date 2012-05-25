1be84bf0126f3b1a10815b2c85b004044c9202f2
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-05-25 12:45:15 -0400

    i added a new script to do crafting and creation.

--- command-denovo-checkpoint.sh
+++ command-denovo-checkpoint.sh
@@ -2,6 +2,7 @@ rm -rf TestX
 
 mpiexec -n $NSLOTS -output-filename TestX \
  ~/Ray \
+-read-write-checkpoints \
  -p ~/data-for-system-tests/strept/200xStreptococcus-pneumoniae-R6.fasta_fragments_1.fasta \
     ~/data-for-system-tests/strept/200xStreptococcus-pneumoniae-R6.fasta_fragments_2.fasta \
 -o TestX 
