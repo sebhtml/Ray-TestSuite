39eaed6b9e996a3953b3a53eb2151e7a0ab2abd0
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-07-26 08:24:21 -0400

    scripts: documented script to clean Linux cache
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- scripts/linux-clean-caches.sh
+++ scripts/linux-clean-caches.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+# run this as root to clean the cache and free memory
+# Author: Sébastien Boisvert
+# useful when running velvetg after velveth on a buggy Linux
+# distribution
+
+# sync everything, brings coherence
 sync
+
 cat /proc/sys/vm/drop_caches > defaultValue
+
+# To free pagecache, dentries and inodes:
 echo 3 > /proc/sys/vm/drop_caches
+
 cat defaultValue > /proc/sys/vm/drop_caches
