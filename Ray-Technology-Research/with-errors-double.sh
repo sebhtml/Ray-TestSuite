65d1738c0e175240a3f8b921a2c1bd574d74b559
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-01-22 17:12:32 -0500

    new system test to check library data
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ Ray-Technology-Research/with-errors-double.sh
@@ -0,0 +1,12 @@
+rm -rf TestX
+
+mpiexec -n $NSLOTS -output-filename TestX \
+ ./Ray \
+-p strept_200_1.fastq \
+strept_200_2.fastq \
+-p strept_200_1.fastq \
+strept_200_2.fastq \
+-o TestX \
+-search DatabaseTest
+
+
