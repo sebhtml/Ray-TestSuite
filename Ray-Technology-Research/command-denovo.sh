60d5c50e8fa0ea3b1ccc3e9c488a78734a3869b7
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-01-22 16:49:16 -0500

    use local executable instead
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- Ray-Technology-Research/command-denovo.sh
+++ Ray-Technology-Research/command-denovo.sh
@@ -1,7 +1,7 @@
 rm -rf TestX
 
 mpiexec -n $NSLOTS -output-filename TestX \
- ~/Ray \
+./Ray \
  -p 200xStreptococcus-pneumoniae-R6.fasta_fragments_1.fasta \
     200xStreptococcus-pneumoniae-R6.fasta_fragments_2.fasta \
 -o TestX 
