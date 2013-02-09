cb2a81e00d47eff645cbd2fceeb015b990a2fe76
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-02-08 19:35:52 -0500

    add some portability for unit tests
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- scripts/load-modules.sh
+++ scripts/load-modules.sh
@@ -1,6 +1,16 @@
-# load modules on colosse
+# load mod /lustre2/clumeq
 
-module load compilers/gcc/4.7.2
-module load mpi/openmpi/1.6.3_gcc
+# colosse.clumeq.ca
+if test -d /lustre2/clumeq
+then
+	module load compilers/gcc/4.7.2
+	module load mpi/openmpi/1.6.3_gcc
+fi
 
+# corbeil-mp2.rqchp.ca
+if test -d /mnt/scratch_mp2
+then
+	module load gcc/4.7.0
+	module load openmpi_gcc64/1.6.3
+fi
 
