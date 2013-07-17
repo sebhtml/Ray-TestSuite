411eee040ac9872f0a30c04671da6a7cce986b49
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-07-17 17:15:10 -0400

    add machinefile
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- Ray-Technology-Research/MiSeq.sh
+++ Ray-Technology-Research/MiSeq.sh
@@ -2,11 +2,14 @@
 
 rm -rf TestX
 
+#-machinefile ~/Machines.txt \
+
 mpiexec -n $NSLOTS \
 	-output-filename TestX \
 ./Ray -k 31 \
 -p \
 MiSeq_Ecoli_MG1655_110527_R1.fastq \
 MiSeq_Ecoli_MG1655_110527_R2.fastq \
+-write-kmers -write-seeds \
 -o TestX \
 &> TestX.stdout \
