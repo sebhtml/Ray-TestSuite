06c96d0129de3cda577da0222caa22d5951edbd0
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-07-29 14:56:08 -0400

    don't write kmers
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- Ray-Technology-Research/MiSeq-Checkpoint.sh
+++ Ray-Technology-Research/MiSeq-Checkpoint.sh
@@ -3,13 +3,13 @@
 rm -rf TestX
 
 rm -rf save.ray/*Seeds*
+#-write-kmers \
 
 mpiexec -n $NSLOTS \
 	-output-filename TestX \
 ./Ray -k 31 \
 -read-write-checkpoints save.ray \
 -write-seeds \
--write-kmers \
 -p \
 MiSeq_Ecoli_MG1655_110527_R1.fastq \
 MiSeq_Ecoli_MG1655_110527_R2.fastq \
