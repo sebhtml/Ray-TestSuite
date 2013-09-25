95f53864da40740d0fbd3bdc216eb02dd81a6a97
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-09-24 20:49:39 -0400

    update allocation number
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- system-tests/Large-Datasets/Ray-ILMN.sh
+++ system-tests/Large-Datasets/Ray-ILMN.sh
@@ -1,17 +1,16 @@
 #PBS -S /bin/bash
-#PBS -N Bird-16
-#PBS -o Bird-16.o
-#PBS -e Bird-16.e
+#PBS -N RayRay-Bird-999
+#PBS -o RayRay-Bird-999.o
+#PBS -e RayRay-Bird-999.e
 #PBS -A nne-790-ac
-#######PBS -A fsi-440-ab
-#PBS -l walltime=05:00:00
+#PBS -l walltime=02:00:00:00
 #PBS -l qos=SPJ1024
-#PBS -l nodes=64:ppn=8
+#PBS -l nodes=128:ppn=8
 
 cd $PBS_O_WORKDIR
 
 module load nne-790-ab/ray/last-build
 
-mpiexec -n 512 Ray Ray-ILMN.conf
+mpiexec -n 1024 Ray Ray-ILMN.conf
 
 
