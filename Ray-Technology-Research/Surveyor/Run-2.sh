8fec7fb3820e11f92b2f0101b9407620ef9e31d3
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-10-24 09:04:47 -0400

    add more tests
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ Ray-Technology-Research/Surveyor/Run-2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ray Surveyor: compare genomic content between samples
+
+rm -rf RaySurveyorResults
+
+#-show-communication-events \
+
+processes=24
+
+rm RaySurveyorResults.*
+
+mpiexec -n $processes \
+	-output-filename RaySurveyorResults \
+./Ray \
+-k 31 \
+-output RaySurveyorResults \
+-run-surveyor \
+-read-sample-graph ID033401 ./Assembly_2013-10-07/1996/ID033401-Ray-2013-10-07/kmers.txt \
+-read-sample-graph ID033406 ./Assembly_2013-10-07/1996/ID033406-Ray-2013-10-07/kmers.txt \
+
