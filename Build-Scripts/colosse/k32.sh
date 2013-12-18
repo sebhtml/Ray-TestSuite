4bd0a1951b118dbe36f54f5a3dacb3920d98a301
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-12-18 11:25:18 -0500

    add data endpoint for colosse
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- Build-Scripts/colosse/k32.sh
+++ Build-Scripts/colosse/k32.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-. ~/load-modules.sh
+module load tools/make/3.82
+module load compilers/gcc/4.8.0
+module load apps/blcr/0.8.4
+module load mpi/openmpi/1.6.4_gcc
+
 
 commit=$(git log|grep ^commit|head -n1|awk '{print $2}')
+version=$commit
+release=1
 
-build=devel-$commit
 
-#module load apps/ray/$build
-
-cd ~/git-clones/ray
-
-make clean
-
+make clean HAVE_LIBZ=y HAVE_LIBBZ2=y 
 make -j 8 MAXKMERLENGTH=32 ASSERT=y HAVE_LIBZ=y HAVE_LIBBZ2=y  CXXFLAGS=" -O3 -std=c++98 -Wall -march=native -g" 
 
-destination=/rap/nne-790-ab/software/nne-790-ab/apps/ray/$build
+prefix=/rap/nne-790-ab/software/nne-790-ab/apps/ray/$version-$release
+
+while test -d $prefix
+do
+	release=$(($release + 1 ))
+	prefix=/rap/nne-790-ab/software/nne-790-ab/apps/ray/$version-$release
+done
+
+build=$version-$release
+destination=$prefix
 
 mkdir -p $destination/bin
 
@@ -25,9 +34,17 @@ modulefile=/rap/nne-790-ab/modulefiles/nne-790-ab/apps/ray/$build
 mkdir -p $modulefile
 rm -rf $modulefile
 
-cp /rap/nne-790-ab/modulefiles/nne-790-ab/apps/ray/devel-template $modulefile
-operation="s=devel-template=$build=g"
+cp ~/git-clones/Ray-TestSuite/Build-Scripts/colosse/Template \
+ $modulefile
+
+operation="s=version=$version=g"
 sed -i $operation $modulefile
 
-echo "Installed in $destination"
+operation="s=release=$release=g"
+sed -i $operation $modulefile
+
+echo "to use the new module: "
+echo ""
+echo "module use /rap/nne-790-ab/modulefiles"
+echo "module load nne-790-ab/apps/ray/$build"
 
